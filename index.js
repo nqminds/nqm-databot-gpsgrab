b191b53973afbe8c0bd8df19bd4b02f4e0023a35
Alex <mereacre@gmail.com>
2016-11-18 10:32:54 +0000

    Added updateDataset and removed debug info

--- index.js
+++ index.js
@@ -5,15 +5,12 @@
  * @param {Object} packageParams of the databot.
  */
 function GrapGPS(tdxApi, output, packageParams) {
-    var updateIDState = {};
-
     tdxApi.getDatasetData(packageParams.gpsSources, null, null, null, function (errSources, sourcesData) {
         if (errSources) {
             output.error("Error GPS sources table: %s", JSON.stringify(errSources));
             process.exit(1);
         } else {
             output.debug("Retrieved GPS sources table: %d entries", sourcesData.data.length);
-
 
             var req = function (gpslist, reqlist, cb) {
 
@@ -25,11 +22,6 @@ function GrapGPS(tdxApi, output, packageParams) {
                 }
 
                 element = reqlist.pop();
-                
-                if(updateIDState[element.ID]=='undefined')
-                    updateIDState[element.ID] = true;
-
-                output.debug("Processing source ID:%d", element.ID);
                 
                 if (element.Src == 'MK' && element.Datatype == 'XML') {
                     request
@@ -56,8 +48,7 @@ function GrapGPS(tdxApi, output, packageParams) {
                                             else if (field['$']['id'] == "3")
                                                 lon = field['current_value'];
                                         });
-                                        
-                                        
+                                                                                
                                         var entry = {
                                             'ID': Number(element.ID),
                                             'timestamp': Number(timestamp),
@@ -66,21 +57,16 @@ function GrapGPS(tdxApi, output, packageParams) {
                                             'ele': Number(result.feed['location'][0]['ele'])
                                         };
 
-                                        tdxApi.addDatasetData(packageParams.gpsDataTable, entry, function (errAdd, resAdd) {
+                                        tdxApi.updateDatasetData(packageParams.gpsDataTable, entry, true, function (errAdd, resAdd) {
                                             if (errAdd) {
                                                 output.error("Error adding entry to dataset: %s", JSON.stringify(errAdd));
                                                 req(gpslist, reqlist, cb);
                                             } else {
                                                 gpslist.push(entry);
 
-                                                tdxApi.updateDatasetData(packageParams.gpsDataTableLatest, entry, updateIDState[element.ID], function (errUpdate, resUpdate) {
+                                                tdxApi.updateDatasetData(packageParams.gpsDataTableLatest, entry, true, function (errUpdate, resUpdate) {
                                                     if (errUpdate) {
-                                                        output.error("Update: %s", JSON.stringify(errUpdate));
-                                                        updateIDState[element.ID] = !updateIDState[element.ID];                                                        
-                                                    } else {
-                                                        output.debug("Update: %s", JSON.stringify(entry));
-
-                                                        if (updateIDState[element.ID]) updateIDState[element.ID] = false;
+                                                        output.error("Update: %s", JSON.stringify(errUpdate));                                                    
                                                     }
 
                                                     req(gpslist, reqlist, cb);
